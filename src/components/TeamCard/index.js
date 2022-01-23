04fb13a15907bc2fceecce4f6be55cc40a117973
kousar <kousarnaazz@gmail.com>
2022-01-23 08:07:17 +0000

    frst

---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,20 @@
+// FIX8: To use Link component, it should be imported
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageURL} = teamDetails
+
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link">
+        <img src={teamImageURL} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
